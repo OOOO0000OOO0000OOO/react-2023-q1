184be09a8da245b4f8e8f0cf0a46d32529da98dc
Kseniia <chrysostomost0000@gmail.com>
2023-04-02 18:09:12 +0300

    fix: UserCard props

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -3,13 +3,13 @@ import { UserCardData } from '../../models/UserCardData';
 
 export default class UserCard extends Component<UserCardData> {
   render() {
-    const { name, level, attack, type, image } = this.props;
+    const { name, date, attack, type, image } = this.props;
 
     return (
       <div data-test="user-card">
         <h3>{name}</h3>
         <p>{type}</p>
-        <p>{level}</p>
+        <p>{date}</p>
         <p>{attack}</p>
         <img src={image} alt={name}></img>
       </div>
